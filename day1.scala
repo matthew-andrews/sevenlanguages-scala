09c4b4dc5ca082359aacbf6f6e08fa8f4c7ce2d2
Matthew Andrews <matt@mattandre.ws>
2013-10-26 02:17:08 +0100

    Remove unnecessary braces and remove debug

--- day1.scala
+++ day1.scala
@@ -39,7 +39,7 @@ class Board(size: Int) {
         if (m == 0) board(n)(n) else board(size - 1 - n)(n)
     }
 
-    def spacer(value: String) = " " * (size.toString.length() - value.length() + 2)
+    def spacer(value: String) = " " * (size.toString.length - value.length + 2)
 
     def boardFull() = board.flatten.count(_ == EMPTY) == 0
 
@@ -47,13 +47,12 @@ class Board(size: Int) {
 
     def anyListAllEqual(lists: List[List[Marker]]): Boolean = lists.foldLeft(false)(_ || allEqual(_))
 
-    def boardWon() = anyListAllEqual(rows()) || anyListAllEqual(cols()) || anyListAllEqual(diags())
+    def boardWon() = anyListAllEqual(rows) || anyListAllEqual(cols) || anyListAllEqual(diags)
 
     def placeMarker(index: Int, marker: Marker): Boolean = {
       val x = index % size
       val y = (index - x) / size
       if (x >= 0 && index < (size * size) && board(x)(y) == EMPTY) {
-        println("(" + x + "," + y + ")")
         board = List.tabulate(size, size) { (m, n) =>
           if (x == m && y == n) marker else board(m)(n)
         }
@@ -66,21 +65,21 @@ class Board(size: Int) {
 
 val game = new Board(3)
 var currentMove = O
-while (!game.boardFull() && !game.boardWon()) {
+while (!game.boardFull && !game.boardWon) {
   currentMove = if (currentMove == X) O else X
   println("\nThe board:\n")
   println(game)
   print("\nIt's " + currentMove + "'s turn! Enter a location: ")
  
-  val index = Console.readInt()
+  val index = Console.readInt
   if (!game.placeMarker(index, currentMove)) {
     println("Invalid location!")    
   }
 }
 
-println()
+println
  
-if (game.boardWon()) {
+if (game.boardWon) {
   println("Player " + currentMove + " wins!")
 } else {
   println("It's a draw!")
